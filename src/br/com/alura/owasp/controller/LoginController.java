1a7f2385d62e49ce4e904cc8e56d4718ffc58809
RafaNercessian <rafanercessian@gmail.com>
2017-09-25 14:45:48 -0300

    Pegando dados do formulario

--- src/br/com/alura/owasp/controller/LoginController.java
+++ src/br/com/alura/owasp/controller/LoginController.java
@@ -2,26 +2,35 @@ package br.com.alura.owasp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.alura.owasp.model.Usuario;
 
 @Controller
 public class LoginController {
-	
-	@RequestMapping("/cadastrar")
-	public String cadastro(Model model){
-		model.addAttribute("usuario", new Usuario());
-		return "cadastro";
-}
-		
-	
-	@RequestMapping(value="/cadastrar", method=RequestMethod.POST)
-	public void cadastro(@ModelAttribute("usuario")Usuario usuario, ModelMap model){
+
+	@RequestMapping(value = "/usuario")
+	public String usuario(Model model) {
+		Usuario usuario = new Usuario();
+		model.addAttribute(usuario);
+		return "cadastrar";
+	}
+
+	@RequestMapping(value = "/adicionaUsuario", method = RequestMethod.POST)
+	public String adicionaUsuario(
+			@ModelAttribute(value = "usuario") Usuario usuario,
+			BindingResult result, RedirectAttributes redirect, Model model) {
+		if (result.hasErrors()) {
+			return "adicionarProduto";
+		}
 		System.out.println(usuario.getEmail());
+		redirect.addFlashAttribute("mensagem",
+				"Produto adicionado com sucesso!");
+		return "redirect:/usuario";
 	}
 
 }
